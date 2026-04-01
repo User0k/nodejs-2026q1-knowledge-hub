a8976f3d3182bd17442a773dffc878fb27356c7d
Pavel Altov <pavel.altov@yahoo.com>
2026-04-01 17:32:33 +0300

    feat: upadete deps, add env conig, swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import * as yaml from 'js-yaml';
+
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const yamlFile = join(process.cwd(), 'doc', 'api.yaml');
+  const yamlContent = readFileSync(yamlFile, 'utf8');
+  const document = yaml.load(yamlContent) as OpenAPIObject;
+  SwaggerModule.setup('doc', app, document);
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
